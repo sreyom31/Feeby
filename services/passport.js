d5f5cff7288bf7e880d5822e56a5a2e2deed74fc
sreyom31 <sreyom31@gmail.com>
2021-12-30 21:40:32 +0530

    changed redirect uri to relative path
    Signed-off-by: sreyom31 <sreyom31@gmail.com>

--- services/passport.js
+++ services/passport.js
@@ -18,7 +18,7 @@ passport.use(
         {
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: "http://localhost:5000/auth/google/callback",
+            callbackURL: "/auth/google/callback"
         },
         (accessToken, refreshToken, profile, done) => {
             User.findOne({googleId: profile.id})
