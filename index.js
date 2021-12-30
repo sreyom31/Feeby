a2e9f3f43237599ff8be70434ef8f5adaea63a21
sreyom31 <sreyom31@gmail.com>
2021-12-30 21:44:56 +0530

    formatting the code
    Signed-off-by: sreyom31 <sreyom31@gmail.com>

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const passport = require("passport");
-const cookieSession = require('cookie-session')
+const cookieSession = require("cookie-session");
 require("./db/mongoose");
 require("./services/passport");
 const authRouter = require("./routes/authRoutes");
@@ -9,13 +9,15 @@ const app = express();
 
 // Middlewares
 app.use(express.json());
-app.use(cookieSession({
-  maxAge: 30 * 24 * 60 * 60 * 1000,
-  keys: [process.env.cookieKey]
-}))
-app.use(passport.initialize())
-app.use(passport.session())
-app.use(authRouter)
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [process.env.cookieKey],
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(authRouter);
 
 const PORT = process.env.PORT || 5000;
 
--- services/passport.js
+++ services/passport.js
@@ -1,35 +1,34 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const User = require('../models/User')
+const User = require("../models/User");
 
 passport.serializeUser((user, done) => {
-    done(null, user.id)
-})
+  done(null, user.id);
+});
 
 passport.deserializeUser((id, done) => {
-    User.findById(id)
-        .then(user => {
-            done(null, user)
-        })
-})
+  User.findById(id).then((user) => {
+    done(null, user);
+  });
+});
 
 passport.use(
-    new GoogleStrategy(
-        {
-            clientID: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: "/auth/google/callback"
-        },
-        (accessToken, refreshToken, profile, done) => {
-            User.findOne({googleId: profile.id})
-                .then((existingUser) => {
-                    if (existingUser) {
-                        done(null, existingUser)
-                    } else {
-                        new User({googleId: profile.id}).save()
-                            .then(user => done(null, user))
-                    }
-                })
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "/auth/google/callback",
+    },
+    (accessToken, refreshToken, profile, done) => {
+      User.findOne({ googleId: profile.id }).then((existingUser) => {
+        if (existingUser) {
+          done(null, existingUser);
+        } else {
+          new User({ googleId: profile.id })
+            .save()
+            .then((user) => done(null, user));
         }
-    )
+      });
+    }
+  )
 );
