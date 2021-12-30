8a07629636f94faed38ffdb79c4defa60ad0d549
sreyom31 <sreyom31@gmail.com>
2021-12-30 18:19:22 +0530

    serializing and deserializing users
    Signed-off-by: sreyom31 <sreyom31@gmail.com>

--- services/passport.js
+++ services/passport.js
@@ -2,6 +2,17 @@ const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const User = require('../models/User')
 
+passport.serializeUser((user, done) => {
+    done(null, user.id)
+})
+
+passport.deserializeUser((id, done) => {
+    User.findById(id)
+        .then(user => {
+            done(null, user)
+        })
+})
+
 passport.use(
     new GoogleStrategy(
         {
