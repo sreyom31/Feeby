036000b5abe4aeaa5fed4dde82b8cc5531a009e9
sreyom31 <sreyom31@gmail.com>
2021-12-30 18:28:48 +0530

    enabling cookies
    Signed-off-by: sreyom31 <sreyom31@gmail.com>

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const passport = require("passport");
+const cookieSession = require('cookie-session')
 require("./db/mongoose");
 require("./services/passport");
 const authRouter = require("./routes/authRoutes");
@@ -8,6 +10,12 @@ const app = express();
 // Middlewares
 app.use(authRouter);
 app.use(express.json());
+app.use(cookieSession({
+  maxAge: 30 * 24 * 60 * 60 * 1000,
+  keys: [process.env.cookieKey]
+}))
+app.use(passport.initialize())
+app.use(passport.session())
 
 const PORT = process.env.PORT || 5000;
 
