93d14963491432e00c141b58a5554ad012dd4a66
sreyom31 <sreyom31@gmail.com>
2021-12-30 15:23:53 +0530

    Refactoring code structure
    Signed-off-by: sreyom31 <sreyom31@gmail.com>

--- index.js
+++ index.js
@@ -1,30 +1,11 @@
 const express = require("express");
-const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
+require('./services/passport')
+const authRouter = require('./routes/authRoutes')
 
-// Middlewares
 const app = express();
 
-passport.use(
-    new GoogleStrategy(
-        {
-            clientID: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: 'http://localhost:5000/auth/google/callback'
-        },
-        (accessToken, refreshToken, profile, done) => {
-            console.log("access token ", accessToken)
-            console.log('refresh token ', refreshToken)
-            console.log('profile: ', profile)
-        }
-    )
-)
-
-app.get('/auth/google', passport.authenticate('google', {
-    scope: ['profile', 'email']
-}))
-
-app.get('/auth/google/callback', passport.authenticate('google'))
+// Middlewares
+app.use(authRouter)
 
 const PORT = process.env.PORT || 5000
 
---
+++ routes/authRoutes.js
@@ -0,0 +1,11 @@
+const { Router } = require('express')
+const passport = require("passport")
+const router = Router()
+
+router.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+}))
+
+router.get('/auth/google/callback', passport.authenticate('google'))
+
+module.exports = router
