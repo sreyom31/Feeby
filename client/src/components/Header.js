9ab66a82279a43d52f2f5fafa6c0aa47ea761a44
sreyom31 <sreyom31@gmail.com>
2021-12-31 22:07:07 +0530

    header content dynamic render
    Signed-off-by: sreyom31 <sreyom31@gmail.com>

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,16 +1,30 @@
 import React from 'react';
+import {connect} from 'react-redux'
 
 function Header(props) {
+    console.log(props)
+    const renderContent = () => {
+        if(props.auth===null) return
+        else if(!props.auth) return (
+            <li><a href="/auth/google">Login With Google</a></li>
+        )
+        else return (<li><a>Logout</a></li>)
+    }
     return (
         <nav>
             <div className="nav-wrapper">
                 <a href="" className="left brand-logo">Feeby</a>
                 <ul id="nav-mobile" className="right">
-                    <li><a href="">Login With Google</a></li>
+                    {/*<li><a href="">Login With Google</a></li>*/}
+                    <li>{renderContent()}</li>
                 </ul>
             </div>
         </nav>
     );
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+    return {auth}
+}
+
+export default connect(mapStateToProps)(Header);
